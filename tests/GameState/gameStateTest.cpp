7f49a296c78749710af8503d048f5f62915829c4
namsonn <namsonn@sfu.ca>
2023-11-17 17:19:58 -0800

    Added more unit test for variant of Map and Vector of DataValue

--- tests/GameState/gameStateTest.cpp
+++ tests/GameState/gameStateTest.cpp
@@ -143,6 +143,13 @@ TEST(DataValueTest, IntValueTest) {
     // Negative int value
     dataValue.setValue(-420);
     EXPECT_EQ(std::get<int>(dataValue.getValueVariant()), -420);
+
+    //Max and min Interger value
+    dataValue.setValue(std::numeric_limits<int>::max());
+    EXPECT_EQ(std::get<int>(dataValue.getValueVariant()), std::numeric_limits<int>::max());
+
+    dataValue.setValue(std::numeric_limits<int>::min());
+    EXPECT_EQ(std::get<int>(dataValue.getValueVariant()), std::numeric_limits<int>::min());
 }
 
 TEST(DataValueTest, BooleanValueTest) {
@@ -190,4 +197,99 @@ TEST(DataValueTest, StringValueTest) {
     std::string stringWithNewlines = "Line 1\nLine 2\nLine 3";
     dataValue.setValue(stringWithNewlines);
     EXPECT_EQ(std::get<std::string>(dataValue.getValueVariant()), stringWithNewlines);
+}
+
+TEST(DataValueTest, MapValueTest) {
+    GS::DataValue dataValue;
+
+    // Test an empty map
+    dataValue.setValue(std::map<std::string, GS::DataValue>{});
+    auto emptyMap = std::get<std::map<std::string, GS::DataValue>>(dataValue.getValueVariant());
+    EXPECT_TRUE(emptyMap.empty());
+
+    //Test map with one entry
+    GS::DataValue intValue;
+    intValue.setValue(1);
+    std::map<std::string, GS::DataValue> sampleMap = {{"Hello World!", intValue}};
+    dataValue.setValue(sampleMap);
+    auto retrievedMap = std::get<std::map<std::string, GS::DataValue>>(dataValue.getValueVariant());
+
+    EXPECT_EQ(retrievedMap.size(), 1);
+
+    //Test map with two entries
+    GS::DataValue firstValue;
+    firstValue.setValue(1);
+
+    GS::DataValue secondValue;
+    secondValue.setValue(2);
+
+    sampleMap = {{"Hello World!", firstValue},
+                {"This is a test", secondValue}};
+    dataValue.setValue(sampleMap);
+    retrievedMap = std::get<std::map<std::string, GS::DataValue>>(dataValue.getValueVariant());
+
+    EXPECT_EQ(retrievedMap.size(), 2);
+
+    //Check if key existed in retrieved map
+    EXPECT_TRUE(retrievedMap.find("Hello World!") != retrievedMap.end());
+    EXPECT_TRUE(retrievedMap.find("This is a test") != retrievedMap.end());
+
+    // Check if a key not in the map is not in the map
+    EXPECT_TRUE(retrievedMap.find("NotInMap") == retrievedMap.end());
+}
+
+TEST(DataValueTest, VectorValueTest){
+    GS::DataValue dataValue;
+
+    //Test an empty vector
+    std::vector<DataValue> valueVector = {};
+    dataValue.setValue(valueVector);
+    auto retrievedVector = std::get<std::vector<DataValue>>(dataValue.getValueVariant());
+    EXPECT_EQ(retrievedVector.size(), 0);
+
+    //Adding one int entry to vector
+    GS::DataValue intValue;
+    intValue.setValue(1);
+    valueVector.emplace_back(intValue);
+    dataValue.setValue(valueVector);
+    retrievedVector = std::get<std::vector<DataValue>>(dataValue.getValueVariant());
+
+    EXPECT_EQ(retrievedVector.size(), 1);
+    EXPECT_EQ(std::get<int>(retrievedVector.at(0).getValueVariant()), std::get<int>(valueVector.at(0).getValueVariant()));
+
+    //Adding one boolean entry to vector
+    GS::DataValue boolValue;
+    boolValue.setValue(true);
+    valueVector.emplace_back(boolValue);
+    dataValue.setValue(valueVector);
+    retrievedVector = std::get<std::vector<DataValue>>(dataValue.getValueVariant());
+
+    EXPECT_EQ(retrievedVector.size(), 2);
+    EXPECT_EQ(std::get<bool>(retrievedVector.at(1).getValueVariant()), std::get<bool>(valueVector.at(1).getValueVariant()));
+
+     //Adding one string entry to vector
+    GS::DataValue stringValue;
+    std::string newString = "Hello World!";
+    stringValue.setValue(newString);
+    valueVector.emplace_back(stringValue);
+    dataValue.setValue(valueVector);
+    retrievedVector = std::get<std::vector<DataValue>>(dataValue.getValueVariant());
+
+    EXPECT_EQ(retrievedVector.size(), 3);
+    EXPECT_EQ(std::get<std::string>(retrievedVector.at(2).getValueVariant()), std::get<std::string>(valueVector.at(2).getValueVariant()));
+
+    //Stress testing ability to hold large number of value in DataValue vector
+
+    // //The vector already have 3 values in it
+    int startingEntry = 3;
+    for (int i = 0; i < 10000; i++) {
+        GS::DataValue currentIntValue;
+        currentIntValue.setValue(i);
+        valueVector.emplace_back(currentIntValue);
+        dataValue.setValue(valueVector);
+        retrievedVector = std::get<std::vector<DataValue>>(dataValue.getValueVariant());
+
+        EXPECT_EQ(std::get<int>(retrievedVector.at(startingEntry + i).getValueVariant()), std::get<int>(valueVector.at(startingEntry + i).getValueVariant()));
+    }
+    EXPECT_EQ(retrievedVector.size(), 10003);
 }
