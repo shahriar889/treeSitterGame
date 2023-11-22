a571176f19820e9c227da1a1ce312d9104d9050b
namsonn <namsonn@sfu.ca>
2023-11-21 16:33:27 -0800

    Refactoring creation of vector into resuable function, dataValueStressTest should run significantly faster now

--- tests/GameState/dataValueStressTest.cpp
+++ tests/GameState/dataValueStressTest.cpp
@@ -14,32 +14,40 @@ std::string getPlaceholder(int index) {
 // Helper function to create a map for stress testing
 std::map<std::string, GS::DataValue> createMapForStressTest(int numEntries) {
     std::map<std::string, GS::DataValue> valueMap;
+
     for (int i = 0; i < numEntries; i++) {
         valueMap.insert(std::make_pair(getPlaceholder(i),
                                         GS::DataValue{ i + 1 }));
     }
+
     return valueMap;
+}
+
+
+// Helper function to create a vector for stress testing
+std::vector<GS::DataValue> createVectorForStressTest(int numEntries){
+    std::vector<GS::DataValue> valueVector;
+
+    for (int i = 0; i < numEntries; i++){
+        valueVector.emplace_back(GS::DataValue{ i });
+    }
+
+    return valueVector;
 }
 
 // Stress testing ability to hold a large number of values in a vector of DataValue
 TEST(DataValueStressTest, VectorValueStressTest) {
     GS::DataValue dataValue;
-    std::vector<DataValue> valueVector;
+    std::vector<DataValue> valueVector = createVectorForStressTest(10000);
     std::vector<DataValue> retrievedVector;
 
-    for (int i = 0; i < 10000; i++) {
-        GS::DataValue currentIntValue;
-        currentIntValue.setValue(i);
-        valueVector.emplace_back(currentIntValue);
-        dataValue.setValue(valueVector);
-    }
-
+    dataValue.setValue(valueVector);
     retrievedVector = std::get<std::vector<GS::DataValue>>(dataValue.getValueVariant());
 
     for (int i = 0; i < 10000; i++) {
         EXPECT_EQ(std::get<int>(retrievedVector.at(i).getValueVariant()), std::get<int>(valueVector.at(i).getValueVariant()));
     }
-
+    
     EXPECT_EQ(retrievedVector.size(), 10000);
 }
 
@@ -51,5 +59,6 @@ TEST(DataValueStressTest, MapValueStressTest) {
 
     dataValue.setValue(valueMap);
     retrievedMap = std::get<std::map<std::string, GS::DataValue>>(dataValue.getValueVariant());
+
     EXPECT_EQ(retrievedMap.size(), 10000);
 }
